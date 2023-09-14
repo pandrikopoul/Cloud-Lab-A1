d26079a18f6ba5002f39f2e1211ca79704e1e0d9
pandrikopoul <124565200+pandrikopoul@users.noreply.github.com>
2023-09-14 21:06:07 +0300

    Cloud_Lab_A1.py

--- Cloud_Lab_A1.py
+++ Cloud_Lab_A1.py
@@ -20,7 +20,7 @@ if response.status_code == 200:
     
     volume_path = "/usr/src/data/save_file_nt" #sys.argv[-1]
     with open(volume_path, "a") as file:
-        file.write(response.text)
+        file.write(response.text + '\n')
 
     print(f"Response saved to {volume_path}")
 
